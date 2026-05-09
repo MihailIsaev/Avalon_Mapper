e9e5b003c6e6c859db3fc3502859b6728c7a63e7
mihail1488 <mihaisaev740@gmail.com>
2026-05-09 15:01:11 +0300

    win fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4364,7 +4364,7 @@ fn run_capture_ocr_with_helper(
 ) -> Result<CaptureOcrResult, String>  {
     #[cfg(target_os = "windows")]
     {
-        return run_capture_ocr(
+        return run_capture_ocr_with_helper(
             helper_path,
             capture_dir,
             paddle_ocr,
